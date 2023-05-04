b6077762fa3599a198fba73b680ea65cc56b7b4d
Mursel Elibol <murselelibol@gmail.com>
2023-05-04 20:05:51 +0300

    init project file and import pod

---
+++ Podfile
@@ -0,0 +1,10 @@
+# platform :ios, '9.0'
+
+target 'swift-movie-tracker-mvvm' do
+
+  use_frameworks!
+  
+  pod 'Alamofire'
+  pod 'SDWebImage', '~> 5.0'
+
+end
---
+++ Pods/Target Support Files/Pods-swift-movie-tracker-mvvm/Pods-swift-movie-tracker-mvvm-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_swift_movie_tracker_mvvm : NSObject
+@end
+@implementation PodsDummy_Pods_swift_movie_tracker_mvvm
+@end
