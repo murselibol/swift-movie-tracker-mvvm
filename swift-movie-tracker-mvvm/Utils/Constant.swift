dd5e3d0f4e502273e09f5f202ccfc26ca285c4fd
Mursel Elibol <murselelibol@gmail.com>
2023-05-07 19:28:05 +0300

    feat: add constant

---
+++ swift-movie-tracker-mvvm/Utils/Constant.swift
@@ -0,0 +1,13 @@
+//
+//  Constant.swift
+//  swift-movie-tracker-mvvm
+//
+//  Created by Mursel Elibol on 7.05.2023.
+//
+
+import Foundation
+
+struct K {
+    static let notFoundMovieImage = "https://upload.wikimedia.org/wikipedia/commons/d/d1/Image_not_available.png"
+}
+
