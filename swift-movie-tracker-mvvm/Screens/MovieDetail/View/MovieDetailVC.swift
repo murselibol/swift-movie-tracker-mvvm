77ff6a827e506860eefd66ba9ebe0da7dcf5b5b0
Mursel Elibol <murselelibol@gmail.com>
2023-05-11 10:10:45 +0300

    feat: create movie detail view controller and view model

---
+++ swift-movie-tracker-mvvm/Screens/MovieDetail/View/MovieDetailVC.swift
@@ -0,0 +1,25 @@
+//
+//  MovieDetailVC.swift
+//  swift-movie-tracker-mvvm
+//
+//  Created by Mursel Elibol on 11.05.2023.
+//
+
+import UIKit
+
+protocol MovieDetailViewInterface {
+    
+}
+
+class MovieDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
+
+extension MovieDetailVC: MovieDetailViewModelInterface {
+    
+}
---
+++ swift-movie-tracker-mvvm/Screens/MovieDetail/ViewModel/MovieDetailVM.swift
@@ -0,0 +1,20 @@
+//
+//  MovieDetailVM.swift
+//  swift-movie-tracker-mvvm
+//
+//  Created by Mursel Elibol on 11.05.2023.
+//
+
+import Foundation
+
+protocol MovieDetailViewModelInterface {
+    
+}
+
+final class MovieDetailVM {
+    
+}
+
+extension MovieDetailVM: MovieDetailViewModelInterface {
+    
+}
