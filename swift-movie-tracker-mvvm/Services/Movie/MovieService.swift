cedf39caaed6e8b97b4d079fe30bf6023c90a5c2
Mursel Elibol <murselelibol@gmail.com>
2023-05-17 22:45:45 +0300

    feat: add get movie request

--- swift-movie-tracker-mvvm/Services/Movie/MovieService.swift
+++ swift-movie-tracker-mvvm/Services/Movie/MovieService.swift
@@ -9,6 +9,7 @@ import Foundation
 
 protocol MovieServiceInterface {
     func getMoviesByCategory(categoryName: MovieCategory, complete: @escaping((MoviesModel?, Error?)->()))
+    func getMovie(id: Int, complete: @escaping((MovieModel?, Error?)->()))
 }
 
 class MovieService: MovieServiceInterface {
@@ -33,6 +34,18 @@ class MovieService: MovieServiceInterface {
         }
         
         NetworkManager.shared.request(type: MoviesModel.self, url: url, method: .get) { response in
+            switch response {
+            case .success(let data):
+                complete(data, nil)
+            case .failure(let error):
+                complete(nil, error)
+            }
+        }
+    }
+    
+    //MARK: - Get
+    func getMovie(id: Int, complete: @escaping ((MovieModel?, Error?) -> ())) {
+        NetworkManager.shared.request(type: MovieModel.self, url: "movie/\(id)", method: .get) { response in
             switch response {
             case .success(let data):
                 complete(data, nil)
